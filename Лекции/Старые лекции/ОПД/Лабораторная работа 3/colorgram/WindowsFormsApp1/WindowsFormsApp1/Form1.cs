f144ee2f4054f791d04538bfbe6df557bbc9d81b
rustam-mansurov <67462400+rustam-mansurov@users.noreply.github.com>
2024-01-10 09:40:46 +0400

    GO!

---
+++ Лекции/Старые лекции/ОПД/Лабораторная работа 3/colorgram/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,244 @@
+﻿using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rayon1DataSet.Узлы". При необходимости она может быть перемещена или удалена.
+            this.узлыTableAdapter.Fill(this.rayon1DataSet.Узлы);
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "rayon1DataSet.Районы". При необходимости она может быть перемещена или удалена.
+            this.районыTableAdapter.Fill(this.rayon1DataSet.Районы);
+            MapSettings();
+        }
+
+        private void MapSettings()
+        {
+            gMapControl1.Bearing = 0;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.GrayScaleMode = true;
+            gMapControl1.MaxZoom = 30;
+            gMapControl1.MinZoom = 1;
+            gMapControl1.PolygonsEnabled = true;
+            gMapControl1.MarkersEnabled = true;
+            gMapControl1.RoutesEnabled = true;
+            gMapControl1.MouseWheelZoomType = MouseWheelZoomType.MousePositionAndCenter;
+            gMapControl1.NegativeMode = false;
+            gMapControl1.ShowTileGridLines = false;
+            gMapControl1.Zoom = 9;
+            gMapControl1.Position = new PointLatLng(56, 52);
+            GMaps.Instance.Mode = AccessMode.ServerOnly;
+            panel1.Hide();
+            color = Color.FromArgb(255,0,255,0);
+        }
+
+        void Card()
+        {
+            if (checkBox2.Checked == true) gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            else gMapControl1.MapProvider = GMapProviders.EmptyProvider;        
+        }
+        void Polygon()
+        {
+            panel1.Refresh();
+            if (checkBox1.Checked == true)
+            {
+                //gMapControl1.Overlays.Clear();
+                panel1.Show();
+                DrawPolygon();
+                gMapControl1.Zoom += 0.0001;
+                gMapControl1.Zoom -= 0.0001;
+                gMapControl1.Refresh();
+            }
+
+            else 
+            {
+                panel1.Hide();
+                gMapControl1.Overlays.Clear();
+                gMapControl1.Refresh();
+            } 
+
+        }
+
+        private void DrawPolygon()
+        {
+            GMapOverlay polygonOverlay = new GMapOverlay("polygon");
+            GMarkerGoogle marker;
+            GMapOverlay markerOverlay = new GMapOverlay("markers");
+            List<PointLatLng> listPoints = new List<PointLatLng>();
+            GMapPolygon mappolygon;
+            int j = 0;
+            //легенда
+            Legend(out Color[] colorPolygon, out double min, out double max, out int grids, out double step);
+            /////////
+            int countItems = dataGridView1.RowCount - 1;
+            int[] v = new int[2];
+            Color colorBoundary = Color.FromArgb(200, 255, 0, 0);
+            while (j < countItems)
+            {
+                listPoints.Clear();
+                v[0] = Convert.ToInt32(dataGridView1[5, j].Value);
+                v[1] = Convert.ToInt32(dataGridView1[6, j].Value);
+                for (int i = v[0] - 1; i < v[1] - 1; i++)
+                {
+                    listPoints.Add(new PointLatLng((Convert.ToDouble(dataGridView3[1, i].Value) / 1000000), Convert.ToDouble(dataGridView3[2, i].Value) / 1000000));
+                }
+                mappolygon = new GMapPolygon(listPoints, "polygon" + j.ToString())
+                {
+                    Stroke = new Pen(colorBoundary)
+                };
+                int n = Getnumber(Convert.ToDouble(dataGridView1[2, j].Value), min, max, grids, step);
+                mappolygon.Fill = new SolidBrush(colorPolygon[n]);
+
+                polygonOverlay.Polygons.Add(mappolygon);
+                marker = new GMarkerGoogle(new PointLatLng(Convert.ToDouble(dataGridView1[3, j].Value) / 1000000,
+                    Convert.ToDouble(dataGridView1[4, j].Value) / 1000000), GMarkerGoogleType.green)
+                {
+                    ToolTipText = Convert.ToString(dataGridView1[1, j].Value),
+                    ToolTipMode = MarkerTooltipMode.Always
+                };
+                /*if (checkBox1.Checked == true)
+                {
+                    marker.ToolTipMode = MarkerTooltipMode.Always;
+                }
+                else
+                {
+                    //gMapControl1.Overlays.Clear();
+                    marker.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+                }*/
+                markerOverlay.Markers.Add(marker);
+                j++;
+            }
+
+            gMapControl1.Overlays.Add(polygonOverlay);
+            gMapControl1.Overlays.Add(markerOverlay);
+
+            /*GMapProviders.GoogleMap.ApiKey = @"AIzaSyBDOSrmPItAIPLL9EjPFh_hDXuw2wO_hwI";//@"AIzaSyCuy98k2KM7vUyF9sI0-R7X68QdIE-YynY";
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            gMapControl1.CacheLocation = @"cache";
+            PointLatLng start = new PointLatLng(0, 0);
+            PointLatLng end = new PointLatLng(1, 1);
+            GetDistanceByRoute(start.Lat, start.Lng, end.Lat, end.Lng);*/
+        }
+
+        private void Legend(out Color[] colorPolygon, out double min, out double max, out int grids, out double step)
+        {
+            int red, green, blue;
+            int hred, hgreen, hblue;
+            Color color1 = Color.FromArgb(255, 255, 255, 255);
+            Pen pen = new Pen(Color.Black,0.002f);
+            grids = 5;
+            int w = panel1.Width / grids;
+            min = 0;
+            max = 100;
+            step = max / grids;
+            Graphics g = panel1.CreateGraphics();
+            Rectangle[] rect = new Rectangle[grids];
+            Brush[] br = new SolidBrush[grids];
+            colorPolygon = new Color[grids];
+            Font arialFont = new Font("Arial", 10);
+            Font timesFont = new Font("Times New Roman", 12);
+            hred = (color1.R - color.R) / grids;
+            hgreen = (color1.G - color.G) / grids;
+            hblue = (color1.B - color.B) / grids;
+            for (int i = 0; i < grids; i++)
+            {
+                red = color1.R - hred * i;
+                green = color1.G - hgreen * i;
+                blue = color1.B - hblue * i;
+                colorPolygon[i] = Color.FromArgb(255, red, green, blue);
+                //colorPolygon[i] = Color.FromArgb(255, 0, 255 - (int)(255 * i * step / max), 0);
+                /*colorPolygon[i] = Color.FromArgb(255, Convert.ToInt32(color.R * (1 - i * step / max)), 
+                    Convert.ToInt32(color.G * (1-i*step/max)), Convert.ToInt32(color.B * (1 - i * step / max)));*/
+                rect[i] = new Rectangle(w * i, 0, w, 20);
+                br[i] = new SolidBrush(colorPolygon[i]);
+                g.FillRectangle(br[i], rect[i]);
+                g.DrawRectangle(pen, rect[i]);
+                g.DrawString(Convert.ToString(step * i), arialFont, Brushes.Blue, w * i, 20);
+            }
+            g.DrawString("Лесистость территории, %    Сменить цвет", timesFont, Brushes.Black, 0, 35);
+        }
+
+        int Getnumber(double load, double min, double max, int grids, double step)
+        {
+            double left, right;
+            int k = -1;
+            for (int i = 0; i < grids; i++)
+            {
+                left = min + step * i;
+                right = left + step;
+                if ((load >= left) && (load < right)) k = i;
+            }
+            if (load <= min) k = 0;
+            if (load >= max) k = grids - 1;
+            return k;           
+        }
+
+        public static double GetDistanceByRoute(double startLat, double startLng, double endLat, double endLng)
+        {
+            GoogleMapProvider.Instance.ApiKey = "AIzaSyCuy98k2KM7vUyF9sI0-R7X68QdIE-YynY";
+
+            //GMap.NET.MapProviders.YandexMapProvider.Instance
+            //GMap.NET.MapProviders.BingMapProvider.Instance. .Instance.ApiKey = "Your Api Key";
+            PointLatLng start = new PointLatLng(startLat, startLng);
+            PointLatLng end = new PointLatLng(endLat, endLng);
+            MapRoute route = GMap.NET.MapProviders.GoogleMapProvider.Instance.GetRoute(start, end, false, false, 15);
+            return route.Distance;
+        }
+
+        void Marshrut(double startLat, double startLng, double endLat, double endLng)
+        {
+            PointLatLng start = new PointLatLng(startLat, startLng);
+            PointLatLng end = new PointLatLng(endLat, endLng);
+            List<PointLatLng> listroute = new List<PointLatLng>
+            {
+                start,
+                end
+            };
+            GMapRoute r = new GMapRoute(listroute, "My route");
+            GMapOverlay routesOverlay = new GMapOverlay("routes");
+            routesOverlay.Routes.Add(r);
+            gMapControl1.Overlays.Add(routesOverlay);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Marshrut(Convert.ToDouble(textBox1.Text)/1000000, Convert.ToDouble(textBox2.Text) / 1000000,
+                Convert.ToDouble(textBox3.Text) / 1000000, Convert.ToDouble(textBox4.Text) / 1000000);           
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            Polygon();
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            Card();
+        }
+
+        Color color;
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            ColorDialog colorDlg = new ColorDialog();
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                color = colorDlg.Color;
+                Polygon();
+            }
+        }
+    }
+}
---
+++ Лекции/Старые лекции/ОПД/Лабораторная работа 3/marshruty/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemmory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(12, 12);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(1099, 523);
+            this.gMapControl1.TabIndex = 0;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Info;
+            this.groupBox1.Controls.Add(this.checkBox5);
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(1003, 322);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(104, 213);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Выберите слои";
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(7, 116);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(64, 17);
+            this.checkBox5.TabIndex = 4;
+            this.checkBox5.Text = "Дороги";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            this.checkBox5.CheckedChanged += new System.EventHandler(this.checkBox5_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(7, 92);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(64, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "Пункты";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(7, 68);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(96, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "Цветограмма";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(7, 44);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(70, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "Границы";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 20);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Подложка";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.panel1.BackColor = System.Drawing.SystemColors.Info;
+            this.panel1.Location = new System.Drawing.Point(428, 476);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(307, 59);
+            this.panel1.TabIndex = 3;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(828, 45);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(279, 193);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1123, 547);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
