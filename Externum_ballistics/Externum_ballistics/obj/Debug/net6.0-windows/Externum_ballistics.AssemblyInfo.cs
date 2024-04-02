f2be5cacc3b244fa1c8bcb7a464f15e8c451aa0c
rustam-mansurov <67462400+rustam-mansurov@users.noreply.github.com>
2024-04-02 18:07:15 +0400

    пхнх

--- Externum_ballistics/Externum_ballistics/obj/Debug/net6.0-windows/Externum_ballistics.AssemblyInfo.cs
+++ Externum_ballistics/Externum_ballistics/obj/Debug/net6.0-windows/Externum_ballistics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Externum_ballistics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d42b8c9618a4148ee09211601d816e4e9f5305f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Externum_ballistics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Externum_ballistics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
